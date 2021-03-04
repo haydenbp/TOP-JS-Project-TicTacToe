2e710826f4e3cf0a99c5e7ea62abf1982a842bee
haydenbp <32523980+haydenbp@users.noreply.github.com>
2021-03-03 22:52:11 -0500

    gameboard functional

--- script.js
+++ script.js
@@ -6,32 +6,80 @@ let board = ['O','x','x','x','x','x','x','x','x']
 //players stored as objects
 
 //gameboard function
-function makeGameBoard(array){
 
-    for(let i = 0; i < array.length; i++){
-        makeGameTile(array[i],i)
+const gameBoard = (() =>{
+
+    const boardDiv = document.getElementById('board')
+    
+    function makeGameBoard(){
+
+        for(let i = 0; i < board.length; i++){
+            makeGameTile(board[i],i)
+        }
+
     }
 
-}
+    function clearBoard() {
+        while (boardDiv.firstChild) {
+            boardDiv.removeChild(boardDiv.firstChild);
+        }
+        makeGameBoard()
+    }
 
-//function to generate individual tiles on board
+    //function to generate individual tiles on board
 
-function makeGameTile(arrayElement,index){
+    function makeGameTile(arrayElement,index){
 
-    const board = document.getElementById('board')
+        
 
-    const gameTile = document.createElement('div')
+        const gameTile = document.createElement('div')
 
-    gameTile.innerText = arrayElement
+        gameTile.innerText = arrayElement
 
-    gameTile.classList = 'gametile';
+        gameTile.classList = 'gametile';
 
-    gameTile.id = `${index}`
+        gameTile.id = `${index}`
 
-    board.appendChild(gameTile,index)
+        gameTile.addEventListener('click',()=>{
+
+            board[index] = 'x';
+
+            clearBoard()
+
+            console.log(board)
+        })
+
+        boardDiv.appendChild(gameTile,index)
+
+        
+
+    }
 
 
-    console.log(arrayElement[index])
-}
 
-makeGameBoard(board)
+    return makeGameBoard()
+
+})();
+
+/*
+How do i interact?
+
+pick X or O
+
+player 1 goes than player two
+
+
+
+what does it need to do?
+
+pick x or o
+
+store game board in array
+
+change the board when interacted with
+
+check to see if anyone wins
+
+make sure you cant play same tile twice
+
+store players*/
